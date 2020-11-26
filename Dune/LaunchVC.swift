c4abba2f5d3c543609e5824f8f596f9fdde4224c
WaylanSands <waylansands@gmail.com>
2020-11-26 21:58:17 +1100

    Updating error message

--- Dune/LaunchVC.swift
+++ Dune/LaunchVC.swift
@@ -131,7 +131,7 @@ class LaunchVC: UIViewController {
     private func sendToIntro() {
         rootVC = IntroVC()
         
-//        DuneDelegate.newRootView(rootVC)
+//        DuneDelegate.newRootView(rootVC) 
         UIApplication.shared.keyWindow?.rootViewController = rootVC
     }
 }
